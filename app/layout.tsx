a235fa4ab142ee39d61a18f556f34e9137726ad0
krishnakantasharma <krishnakantasharma@gmail.com>
2024-10-15 13:44:16 +0530

    End of Chapter 16

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,15 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'Next.js Dashboard based on official course, built with App Router.',
+  metadataBase: new URL('https://nextjs-dashboard-latest.vercel.app'),
+};
 
 export default function RootLayout({
   children,
